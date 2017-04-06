d27dbe6b12c9a12ca16c10ce36faff8fe592d2ae
w205 <w205@ip-172-31-62-200.ec2.internal>
2017-04-06 01:45:40 +0000

    2nd commit

--- Exercise2/extweetwordcount/create_psql_table.py
+++ Exercise2/extweetwordcount/create_psql_table.py
@@ -1,6 +1,3 @@
-#Sample code snippets for working with psycopg
-
-
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
@@ -13,6 +10,7 @@ try:
     # CREATE DATABASE can't run inside a transaction
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cur = conn.cursor()
+    cur.execute("DROP DATABASE IF EXISTS tcount")
     cur.execute("CREATE DATABASE tcount")
     cur.close()
     conn.close()
@@ -31,36 +29,7 @@ cur.execute('''CREATE TABLE tweetwordcount
        (word TEXT PRIMARY KEY     NOT NULL,
        count INT     NOT NULL);''')
 conn.commit()
-
-
-#Running sample SQL statements
-#Inserting/Selecting/Updating
-
-#Rather than executing a whole query at once, it is better to set up a cursor that encapsulates the query, 
-#and then read the query result a few rows at a time. One reason for doing this is
-#to avoid memory overrun when the result contains a large number of rows. 
-
-cur = conn.cursor()
-
-#Insert
-cur.execute("INSERT INTO tweetwordcount (word,count) \
-      VALUES ('test', 1)");
-conn.commit()
-
-#Using variables to update
-uCount=5
-uWord="test"
-cur.execute("UPDATE tweetwordcount SET count=%s WHERE word=%s", (uCount, uWord))
-conn.commit()
-
-#Select
-cur.execute("SELECT word, count from tweetwordcount")
-records = cur.fetchall()
-for rec in records:
-   print "word = ", rec[0]
-   print "count = ", rec[1], "\n"
-conn.commit()
-
 conn.close()
+
 
 
--- Exercise2/extweetwordcount/src/bolts/wordcount.py
+++ Exercise2/extweetwordcount/src/bolts/wordcount.py
@@ -3,7 +3,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 from collections import Counter
 from streamparse.bolt import Bolt
 
-
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 class WordCounter(Bolt):
 
@@ -13,16 +14,28 @@ class WordCounter(Bolt):
     def process(self, tup):
         word = tup.values[0]
 
-        # Write codes to increment the word count in Postgres
-        # Use psycopg to interact with Postgres
-        # Database name: Tcount 
-        # Table name: Tweetwordcount 
-        # you need to create both the database and the table in advance.
-        
+	#Connecting to tcount
+	conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+	conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+	cur = conn.cursor()
+	conn.commit()
 
         # Increment the local count
         self.counts[word] += 1
         self.emit([word, self.counts[word]])
+	
+	# Insert Word and Count into tweetwordcount
+	cur.execute("SELECT word, count FROM tweetwordcount WHERE word=%s", (word,))
+	records = cur.fetchone()
+        if records == None:
+		cur.execute("INSERT INTO tweetwordcount (word, count) VALUES (%s, %s)", (word, self.counts[word]))
+		conn.commit()
+		
+	else:
+		cur.execute("UPDATE tweetwordcount SET count=%s WHERE word=%s", (self.counts[word], word))
+		conn.commit()	
 
         # Log the count - just to see the topology running
         self.log('%s: %d' % (word, self.counts[word]))
+
+
